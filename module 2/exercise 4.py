b04c2752a5657161bf273c3386e22b55e1618c89
tamseelaa <tamseelm@metropolia.fi>
2024-08-22 11:57:44 +0300

    Add module 2 files

---
+++ module 2/exercise 4.py
@@ -0,0 +1,10 @@
+print("Find the sum,multiply and average of the following asked numbers ")
+x=int(input("enter the 1st name"))
+y=int(input("enter the 2nd name"))
+z=int(input("enter the 3rd name"))
+Sum =x+y+z
+multiply=x*y*z
+average=Sum/3
+print(f"The sum is {Sum}")
+print(f"The multiply is {multiply}")
+print(f"The average is,{average}")
