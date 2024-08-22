190d24fc9ecf15a6ddc10d952d453a15944e951d
tamseelaa <tamseelm@metropolia.fi>
2024-08-22 11:12:17 +0300

    Add module 2 files

---
+++ module 2/exercise 2 part 3.py
@@ -0,0 +1,10 @@
+length_str =input("enter length of rectangle")
+width_str =input("enter width of rectangle")
+length = int (length_str)
+width = int(width_str)
+print(f"length : {length}")
+print(f"width : {width}")
+area=length*width
+print(f"area of rectangle is {area}")
+perimeter= (length*2)+(width*2)
+print(f"perimeter of rectangle is {perimeter}")
