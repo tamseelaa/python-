9939684a2a3f394c238d0633a188b34c7aaccf42
tamseelaa <tamseelm@metropolia.fi>
2024-08-23 10:00:31 +0300

    Add module 2 files

---
+++ module 2/exercise 5.py
@@ -0,0 +1,9 @@
+print("enter a mass in medieval units")
+t= int(input("enter talent:"))
+p= int(input("enter pound:"))
+l= float(input("enter lot:"))
+int_lot= 13.3
+pound= 32*13.3*p
+talent= 20*32*13.3*t
+lot= 13.3*l
+print(f"The weight in modern units:{(pound+talent+lot)/1000:8f}kilogram and {(pound+talent+lot)/1000:.5f}grams" )
