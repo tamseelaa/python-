e1be48bfa30728f5cfa0a7a021d2d3f0bd67dae2
tamseelaa <tamseelm@metropolia.fi>
2024-08-26 14:50:21 +0300

    Add module 2 files

---
+++ module 2/exercise 6.py
@@ -0,0 +1,10 @@
+
+import random
+
+num1=random.randint(100,999)
+print(f"{num1}")
+num2=random.randint(1,6)
+num3=random.randint(1,6)
+num4=random.randint(1,6)
+num5=random.randint(1,6)
+print((num2*1000)+(num3*100)+(num4*10)+num5)
